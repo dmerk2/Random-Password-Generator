e2281b72491a14afee68d405d1bbb279be722131
Daniel Merkin <dan.merkin@gmail.com>
2022-07-05 10:53:09 -0400

    creating correct file and folder structures

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+for(var i = 0; i = passwordText.length; i++) {
+  writePassword += passwordText.charAt(Math.floor(Math.random() * i))
+}
+
   passwordText.value = password;
 
 }
+console.log(writePassword)
+console.log(generatePassword())
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
