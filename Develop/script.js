1e47692a3b05d37ee56d5018ff6bc334bee0520c
Daniel Merkin <dan.merkin@gmail.com>
2022-07-06 15:23:36 -0400

    created if statements if user wants uppercase, lowercase, or special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,35 @@
-// Assignment code here
+// Assignment Code
+var chars = window.prompt("Choose between 8 and 128 characters for your password");
+var uppercase = window.confirm("Do you want uppercase letters?");
+var lowercase = window.confirm("Do you want lowercase letters?");
+var specialChars = window.confirm("Do you want special characters?");
 
+console.log(chars);
+
+if (uppercase === true) {
+  console.log("Uppercase letters")
+}
+
+if (lowercase === true) {
+  console.log("Lowercase letters")
+}
+
+if (specialChars === true) {
+  console.log("Special characters")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-for(var i = 0; i = passwordText.length; i++) {
-  writePassword += passwordText.charAt(Math.floor(Math.random() * i))
-}
-
+  if (!chars) {
+    return;
+  }
   passwordText.value = password;
-
 }
-console.log(writePassword)
-console.log(generatePassword())
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Password-Generator
+# Random Password Generator
+
+## Random Password Generator Mock-Up Image
+
+The following image shows the web application's appearance and functionality:
+
+![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
